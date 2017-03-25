251341e486e1ce239d95898a34a97ddb7c8d0540
HUANG PI-LING <lindarunesugar@yahoo.com.tw>
2017-03-25 18:43:34 +0800

    Initial Commit

---
+++ 期末/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  期末
+//
+//  Created by Huang Pi-Ling on 2017/3/25.
+//  Copyright © 2017年 Huang Pi-Ling. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
